31acc5a257e8fb87f5ef41a0fb343cd33c50f99e
revenantonthemission <corpseonthemission@icloud.com>
2024-07-15 21:18:44 +0900

    async read() test

--- src/helpers/linkUtils.js
+++ src/helpers/linkUtils.js
@@ -30,15 +30,16 @@ function extractLinks(content) {
   ];
 }
 
-function getGraph(data) {
+ function getGraph(data) {
   let nodes = {};
   let links = [];
   let stemURLs = {};
   let homeAlias = "/";
-  (data.collections.note || []).forEach((v, idx) => {
+  (data.collections.note || []).forEach(async (v, idx) => {
     let fpath = v.filePathStem.replace("/notes/", "");
     let parts = fpath.split("/");
     let group = "none";
+    const templateObject = await v.template.read();
     if (parts.length >= 3) {
       group = parts[parts.length - 2];
     }
@@ -51,7 +52,7 @@ function getGraph(data) {
         v.data["dg-home"] ||
         (v.data.tags && v.data.tags.indexOf("gardenEntry") > -1) ||
         false,
-      outBound: extractLinks(v.template.frontMatter.content),
+      outBound: extractLinks(templateObject.frontMatter.content),
       neighbors: new Set(),
       backLinks: new Set(),
       noteIcon: v.data.noteIcon || process.env.NOTE_ICON_DEFAULT,
